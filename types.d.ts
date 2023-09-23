4ff2a3f3d14a8eda193878b148ca320ba924777a
Rohit <rohitmondall8000@gmail.com>
2023-09-23 18:28:30 +0530

    .

---
+++ types.d.ts
@@ -0,0 +1,22 @@
+type smallProjectsDataTypes = {
+    title: string
+    githubLink: string
+    liveLink: string
+    abt: string
+    icon: LucideIcon
+}
+
+type projectsDataType = {
+    title: string,
+    desc: string,
+    img: StaticImageData,
+    featuresList: Array<string>,
+    liveLink: string,
+    githubLink: string
+}
+
+type socialLinksType = {
+    title: string,
+    icon: LucideIcon,
+    link: string
+}
